00f6a303f55842a965bcb7c8554d988d8b281b89
VukDamnjanovic <81642083+WolfFKSG@users.noreply.github.com>
2024-10-14 10:12:01 +0200

    Implemented recommended flights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatIcon, RouterLink],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
