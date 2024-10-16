b445dc310746e7e7ec8f0d6724cf62d9e5521ab4
VukDamnjanovic <81642083+WolfFKSG@users.noreply.github.com>
2024-10-16 14:12:08 +0200

    Updated service and added search comp

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,37 +1,44 @@
 import { NgFor, NgIf } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { MatListModule} from '@angular/material/list'; 
 import { FlightModel } from '../models/flight.model';
-import { PageModel } from '../models/page.model';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { WebService } from '../../services/web.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor, RouterLink, MatListModule, MatFormFieldModule],
+  imports: [MatCardModule, MatButtonModule, HttpClientModule, NgIf, NgFor, RouterLink, MatListModule, MatInputModule, MatSelectModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit{
 
-  public client: HttpClient 
+  public service: WebService
   public recommended: FlightModel[]= []  
-  constructor(private httpClient: HttpClient) {
-    this.client = httpClient
+  public destinations: string[] = []
+
+  // public destinations: string[] = [
+  //   'Malta', 'Memmingen', 'Vienna'
+  // ]
+  public airlines: string[] = [
+    'Air Serbia', 'Fly Emirates', 'Lufthansa'
+  ]
+  public flightClass: string[] = [
+    'First Class', 'Business', 'Economy'
+  ]
+
+  constructor() {
+    this.service = new WebService()
   }
   ngOnInit(): void {
-    const url =
-    'https://flight.pequla.com/api/flight?page=0&size=3&type=departure&sort=scheduledAt,desc';
-  this.client.get<PageModel<FlightModel>>(url, {
-      headers: {
-        'Accept': 'application/json',
-      }
-    })
-    .subscribe((rsp) => (this.recommended = rsp.content));
+    this.service.getRecommendedFlights().subscribe(rsp => this.recommended = rsp.content)
+    this.service.getAvailableDestinations().subscribe(rsp => this.destinations = rsp)
   }
 
   public generateImageUrl( dest: string) {
